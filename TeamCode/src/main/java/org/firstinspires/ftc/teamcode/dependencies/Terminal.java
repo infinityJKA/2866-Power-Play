f98164a0d479f8a19776e56c5cb4c9eae9a251d4
Ash-Greninja101 <80419579+Ash-Greninja101@users.noreply.github.com>
2022-11-12 20:06:57 -0600

    LinearSlide and claw code added to auto not tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dependencies/Terminal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dependencies/Terminal.java
@@ -1,19 +1,21 @@
 package org.firstinspires.ftc.teamcode.dependencies;
 
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.enums.AllianceSide;
 import org.firstinspires.ftc.teamcode.enums.ColorSide;
 import org.firstinspires.ftc.teamcode.enums.Direction;
 import org.firstinspires.ftc.teamcode.enums.Parking;
+import org.firstinspires.ftc.teamcode.dependencies.LinearSlide.LinearPosition;
 
 public class Terminal {
     private static DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, linearSlide;
+    private static Servo claw;
 
     private static BNO055IMU imu;
 
@@ -26,6 +28,7 @@ public class Terminal {
         imu = linearOpMode.hardwareMap.get(BNO055IMU.class, "imu");
         RobotParameters rP = new RobotParameters(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor, 1440, 2.6, 60, linearSlide, imu, 12.5);
         MecanumEncoder mecanumEncoder = new MecanumEncoder(rP, linearOpMode);
+        LinearSlide linSlide = new LinearSlide(linearSlide, claw, linearOpMode);
         Direction direction = null, rotation = null;
 
         ColorSensor colorSensor = new ColorSensor("LiveLeak", linearOpMode.hardwareMap, linearOpMode);
@@ -62,23 +65,23 @@ public class Terminal {
             default:
                 throw new IllegalArgumentException("Position must be LEFT or RIGHT!");
         }
-//         int rotator = allianceSide.LEFT ? -1:1;
+//        int rotator = allianceSide.LEFT ? -1:1;
         if (linearOpMode.opModeIsActive()) {
             mecanumEncoder.moveInches(direction.FORWARD, 54.5, 1);
-            for (int i = 0; i < 3; i++){
+            for (int i = 1; i < 4; i++){
                 mecanumEncoder.rotateDegrees(rotation.CCW, 90 * rotator, 1);
                 mecanumEncoder.moveInches(direction.FORWARD, 3.25, 1);
                 mecanumEncoder.rotateDegrees(rotation.CW, 90 * rotator, 1);
                 mecanumEncoder.moveInches(direction.FORWARD, 3.25, 1);
-                //            linearSlide.moveToPosition(LinearPosition.THREE, 1);
+                linSlide.placeCone(LinearPosition.THREE, 1);
                 mecanumEncoder.moveInches(direction.BACKWARD, 3.25, 1);
                 mecanumEncoder.rotateDegrees(rotation.CW, 90 * rotator, 1);
                 mecanumEncoder.moveInches(direction.FORWARD, 29, 1);
                 mecanumEncoder.moveInches(direction.BACKWARD, 29, 1);
                 mecanumEncoder.rotateDegrees(rotation.CCW, 90 * rotator, 1);
+                LinearPosition coneEnum = LinearPosition.valueOf("CONE"+Integer.toString(i));
+                linSlide.pickupCone(coneEnum, 1);
             }
-//            linearSlide.moveToPosition(LinearPosition.ONE, 1);
-
 
         }
 
